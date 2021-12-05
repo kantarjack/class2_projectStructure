67d68dcef865fcf995402b3ba4b0f90560e6e388
Jakkala <SJakkala@cardlytics.com>
2021-12-05 17:19:36 +0530

    stage1_load_save_data

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="src",
     version="0.0.1",
-    author="c17hawke",
+    author="sashi",
     description="A small package for dvc ml pipeline demo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/c17hawke/dvc-ML-demo-AIOps",
-    author_email="sunny.c17hawke@gmail.com",
+    url="https://github.com/kantarjack/class2_projectStructure",
+    author_email="s.jakkala@gmail.com",
     packages=["src"],
     python_requires=">=3.7",
     install_requires=[
---
+++ src/load_save.py
@@ -0,0 +1,29 @@
+import argparse
+from os import sep
+import pandas as pd
+from src.utils.all_utils import read_yaml,create_directory
+import os
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+    remote_path = config['data_source']
+    data = pd.read_csv(remote_path,sep=';')
+    # save dataset in the local directory
+    # create path to directory: artifacts/raw_local_dir/data.csv
+    artifacts_dir = config["artifacts"]['artifacts_dir']
+    raw_local_dir = config["artifacts"]['raw_local_dir']
+    raw_local_file = config["artifacts"]['raw_local_file']
+    #print(artifacts_dir,raw_local_dir,raw_local_file)
+    raw_local_dir_path = os.path.join(artifacts_dir, raw_local_dir)
+    create_directory(dirs = [raw_local_dir_path])
+    raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
+    print(raw_local_file_path)
+    data.to_csv(raw_local_file_path, sep=",", index=False)
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', "-c", default="config/config.yaml")
+
+args = parser.parse_args()
+get_data(config_path = args.config)
